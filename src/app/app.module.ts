41c694b499419a66857c55cc8fc6765751d18659
Cristian Garcia <crigar888@gmail.com>
2022-02-04 19:05:01 +0100

    default ball created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BoxComponent } from './box/box.component';
+import { BallComponent } from './ball/ball.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxComponent,
+    BallComponent
   ],
   imports: [
     BrowserModule
