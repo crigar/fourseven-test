2e4273911c6a849d889f3f4087f78a2aaaf2aa89
Cristian Garcia <crigar888@gmail.com>
2022-02-06 20:06:40 +0100

    readme added

--- src/app/components/ball/ball.component.ts
+++ src/app/components/ball/ball.component.ts
@@ -21,18 +21,18 @@ export class BallComponent implements OnInit {
   constructor(private colorService: ColorService) { }
 
   ngOnInit(): void {
-    this.getColor();
+    this.getColor(true);
   }
 
-  getColor() {
+  getColor(init: boolean) {
     this.colorService.getColor()
       .subscribe((data: any) => {
         this.ballColor = '#' + data.colors[0].hex;
+        if(!init) this.moveBall();
       });
   }
 
   moveBall() {
-    this.getColor();
     this.className = this.movementClass[this.currentCorner % 4];
     this.currentCorner++;
   }
